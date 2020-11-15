18868d431b2b07cedec198f593658d7d00a43853
TaylorDev <Twitt15@uab.edu>
2020-11-14 18:28:21 -0600

    Drone Flight (Drone Library Repo)

---
+++ src/controllers/PhysicalDrone.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import java.io.IOException;
+
+public abstract class PhysicalDrone {
+	
+	DroneController controller;
+	
+	/***
+	 * 
+	 * @param speed
+	 * @throws IOException 
+	 */
+	public abstract void setSpeed(int speed) throws IOException;
+	
+	/***
+	 * 
+	 * @return
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	public abstract double getSpeed() throws NumberFormatException, IOException;
+	
+	/***
+	 * 
+	 * @return
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	public abstract int getBattery() throws NumberFormatException, IOException;
+	
+}
